5ba96575b17870ad3df372f545206344273d39d7
venkataRamanth <arjivenkataramanthkumar@gmail.com>
2023-01-18 20:41:38 +0530

    reverted adn added

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 this is deploy file inn git practise
+<<<<<<< HEAD
 second edit
 direcxt commit and ad
 entering for git reset
 
+=======
+>>>>>>> parent of d7e2c70 (updated deploy add and commit once)
