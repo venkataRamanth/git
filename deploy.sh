a8af429b6c9e0d132a3c14e81fdcc7fc00caf5c2
venkataRamanth <arjivenkataramanthkumar@gmail.com>
2023-01-18 22:13:30 +0530

    stash applied

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ this is deploy file inn git practise
 second edit
 direcxt commit and ad
 entering for git reset
-updating in uat bramch
+updating in uat branch
+
+edited for gitstash
 
 =======
 >>>>>>> parent of d7e2c70 (updated deploy add and commit once)
